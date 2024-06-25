cd8f6da951a1b9e98703f7cbfc4a92a332f6eb02
KhoDis <khodisdev@gmail.com>
2024-06-25 15:37:39 +0300

    feat: add extra info to education

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -5,6 +5,7 @@ const education = [
     period: "Сентябрь 2020 по Август 2024",
     location: "Санкт-Петербург",
     link: "https://itmo.ru/ru/viewfaculty/7/fakultet_informacionnyh_tehnologiy_i_programmirovaniya.htm",
+    extra: "Из веба получено опыта: СУБД, Java (+Spring), Kotlin, сети, Vue, FreeMarker, JQuery",
     image: "ITMO.png",
   },
 ];
@@ -15,19 +16,38 @@ type EducationProps = {
   period: string;
   location: string;
   link: string;
+  extra: string;
   image: string;
-}
+};
 
-const EducationHero = ({ institution, degree, period, location, link, image }: EducationProps) => (
+const EducationHero = ({
+  institution,
+  degree,
+  period,
+  location,
+  link,
+  extra,
+  image,
+}: EducationProps) => (
   <div className="hero">
     <div className="hero-content flex-col lg:flex-row items-start">
-      <img src={image} className="max-w-sm rounded-lg shadow-2xl" alt={institution} />
+      <img
+        src={image}
+        className="max-w-sm rounded-lg shadow-2xl"
+        alt={institution}
+      />
       <div className="ml-8 space-y-4">
         <h1 className="text-5xl font-bold">{institution}</h1>
         <p>{degree}</p>
         <p>{location}</p>
         <p>{period}</p>
-        <a href={link} target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-sm">
+        <p>{extra}</p>
+        <a
+          href={link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-primary btn-sm"
+        >
           О кафедре
         </a>
       </div>
@@ -46,6 +66,7 @@ const Education = () => {
           period={edu.period}
           location={edu.location}
           link={edu.link}
+          extra={edu.extra}
           image={edu.image}
         />
       ))}
